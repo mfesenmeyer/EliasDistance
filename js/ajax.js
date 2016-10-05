21a8efcda1e502676c8ec81f6448260fdb6c1ae0
Mark Fesenmeyer <markfesenmeyer@Marks-MacBook-Air.local>
2016-10-04 21:43:37 -0400

    Application Screener added

---
+++ js/ajax.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+$("#submit").click(function(){
+var company = $("#company").val();
+
+var dataString = 'company='+ company;
+
+// Returns successful data submission message when the entered information is stored in database.
+if(company=='')
+{
+alert("ALERT: Please type in a company name");
+}
+else
+{
+// AJAX Code To Submit Form.
+$.ajax({
+type: "POST",
+url: "/php/screener.php",
+data: dataString,
+cache: false,
+success: function(result){
+$("#results-table").css("display", "block");
+alert(result);
+}
+});
+}
+return false;
+});
+});
